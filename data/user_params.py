b2d2472cc7d5ea6c3d0494459269d17dcf91b32b
ASundus <48042931+ASundus@users.noreply.github.com>
2019-10-25 11:06:14 -0400

    app

---
+++ data/user_params.py
@@ -0,0 +1,72 @@
+ 
+# This file is auto-generated from a Python script that parses a PhysiCell configuration (.xml) file.
+#
+# Edit at your own risk.
+#
+import os
+from ipywidgets import Label,Text,Checkbox,Button,HBox,VBox,FloatText,IntText,BoundedIntText,BoundedFloatText,Layout,Box
+    
+class UserTab(object):
+
+    def __init__(self):
+        
+        micron_units = Label('micron')   # use "option m" (Mac, for micro symbol)
+
+        constWidth = '180px'
+        tab_height = '500px'
+        stepsize = 10
+
+        #style = {'description_width': '250px'}
+        style = {'description_width': '25%'}
+        layout = {'width': '400px'}
+
+        name_button_layout={'width':'25%'}
+        widget_layout = {'width': '15%'}
+        units_button_layout ={'width':'15%'}
+        desc_button_layout={'width':'45%'}
+
+        param_name1 = Button(description='random_seed', disabled=True, layout=name_button_layout)
+        param_name1.style.button_color = 'lightgreen'
+
+        self.random_seed = IntText(
+          value=0,
+          step=1,
+          style=style, layout=widget_layout)
+
+        units_button1 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button1.style.button_color = 'lightgreen'
+
+        desc_button1 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button1.style.button_color = 'lightgreen'
+
+        row1 = [param_name1, self.random_seed, units_button1, desc_button1] 
+
+        box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='100%')
+        box1 = Box(children=row1, layout=box_layout)
+
+        self.tab = VBox([
+          box1,
+        ])
+
+    # Populate the GUI widgets with values from the XML
+    def fill_gui(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//user_parameters')  # find unique entry point
+        self.random_seed.value = int(uep.find('.//random_seed').text)
+
+
+    # Read values from the GUI widgets to enable editing XML
+    def fill_xml(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//user_parameters')  # find unique entry point
+        uep.find('.//random_seed').text = str(self.random_seed.value)
